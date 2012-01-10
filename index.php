615c466405923659e80e27931cae3c1779231581
Vitaliy Kerib <mixion09@gmail.com>
2012-01-10 23:11:50 +0200

    fix url

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require 'yt.php';
 $manager = new Youtube_Tools();
-/*
+
 # Get information about video
 $manager->id = 'SqhWFk5wts4';
 $manager->get_video_info();
@@ -16,20 +16,19 @@ print_r($manager->get_data());
 echo '</pre>';
 
 # Get video links  (must be used $manager->get_video_info())
-$manager->id = '14jzmgji16Q';
+$manager->id = 'SqhWFk5wts4';
 echo '<pre>';
 print_r($manager->get_links());
 echo '</pre>';
-*/
+
 # Save video (must be used $manager->get_video_info() or $manager->get_links())
-$manager->id = '14jzmgji16Q';
+$manager->id = 'SqhWFk5wts4';
 $manager->get_video_info();
 $path = realpath(dirname(__FILE__));
 $manager->save('mp4-640x360', $path);
-/*
+
 # Video search
 echo '<pre>';
 print_r($manager->search('Ukraine'));
 echo '</pre>';
-*/
 ?>
