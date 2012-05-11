14250a7cb0598fc30068ad9b633fd4329a951e6c
Vitaliy Keryb <mixlion09@gmail.com>
2012-05-11 13:59:17 +0300

    Fixed bugs, change parsing links method

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+require 'yt.php';
+# Get information about video
+YT::init('Sd3diED-M0c');
+echo '<pre>';
+print_r(YT::get_info());
+
+# Get video data
+YT::init('Sd3diED-M0c');
+echo '<pre>';
+print_r(YT::get_data());
+echo '</pre>';
+# Get video links
+YT::init('Sd3diED-M0c');
+echo '<pre>';
+print_r(YT::get_links());
+echo '</pre>';
+# Save video
+YT::init('SqhWFk5wts4');
+YT::save('mp4-640x360', '.', 'Welcome to Ukraine');
+
+# Video search
+echo '<pre>';
+print_r(YT::search('Ukraine'));
+echo '</pre>';
+
+# Get video and output to browser
+YT::init('SqhWFk5wts4');
+YT::get('flv-640x360');
